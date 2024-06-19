5cd8bc475d3384938fc8722d86a523a33c80914e
Objecteur de Croissance <nogrow@pop-os.localdomain>
2024-06-19 08:03:26 +0200

    Add pagination

--- scrape.py
+++ scrape.py
@@ -36,50 +36,60 @@ if __name__ == '__main__':
     if not os.path.isdir(output_dir):
         os.makedirs(output_dir)
     pictures_page = requests.get(album_url)
-
-    print('\r\n==> Processing album')
-    needs_paging = True
-    previous_img_ref = 0
-    while needs_paging:
-        pictures_soup = BeautifulSoup(pictures_page.content, 'html.parser')
-        pictures = pictures_soup.find_all('div', class_='photo-list-photo-view')
-        last_img_ref = get_last_img_ref(pictures)
-        if last_img_ref != 0:
-            pictures_page = requests.get('{}/with/{}'.format(album_url, last_img_ref))
-            if previous_img_ref != last_img_ref:
-                previous_img_ref = last_img_ref
-                previous_pictures = pictures
-            else:
-                needs_paging = False
-        else:
-            needs_paging = False
-            pictures = previous_pictures
+    pages_soup = BeautifulSoup(pictures_page.content, 'html.parser')
+    try:
+        pages = int(pages_soup.find('div', class_='pagination-view').find_all('span')[-2].text)
+    except IndexError:
+        pages = 1
 
     total_ok = 0
     total_fail = 0
-    for picture in pictures:
-        img_ref = get_img_ref(picture)
-        img_path = '{}/{}.jpg'.format(output_dir, img_ref)
-        if os.path.isfile(img_path):
-            total_ok += 1
-            print('    > Picture {} already axists'.format(img_ref))
-        else:
-            img_sizes_url = '{}/{}{}'.format(base_url, img_ref, '/sizes/l/')
-            sizes_page = requests.get(img_sizes_url)
-            sizes_soup = BeautifulSoup(sizes_page.content, 'html.parser')
-            sizes = sizes_soup.find('ol', class_='sizes-list')
-            bigger_sizes = sizes.find_all('li')[-2]
-            try:
-                biggest_size_url = 'https://flickr.com{}'.format(bigger_sizes.find('a').attrs['href'])
-                biggest_page = requests.get(biggest_size_url)
-                biggest_soup = BeautifulSoup(biggest_page.content, 'html.parser')
-                img_url = biggest_soup.find('div', id='allsizes-photo').find('img').attrs['src']
-                img_data = requests.get(img_url).content
-                with open(img_path, 'wb') as handler:
-                    handler.write(img_data)
+
+    for page in range(1, pages + 1):
+        print('\r\n==> Processing album - page {}'.format(page))
+        needs_paging = True
+        previous_img_ref = 0
+        while needs_paging:
+            pictures_soup = BeautifulSoup(pictures_page.content, 'html.parser')
+            pictures = pictures_soup.find_all('div', class_='photo-list-photo-view')
+            last_img_ref = get_last_img_ref(pictures)
+            if last_img_ref != 0:
+                pictures_page = requests.get('{}/with/{}'.format(album_url, last_img_ref))
+                if previous_img_ref != last_img_ref:
+                    previous_img_ref = last_img_ref
+                    previous_pictures = pictures
+                else:
+                    needs_paging = False
+            else:
+                needs_paging = False
+                pictures = previous_pictures
+
+        for picture in pictures:
+            img_ref = get_img_ref(picture)
+            img_path = '{}/{}.jpg'.format(output_dir, img_ref)
+            if os.path.isfile(img_path):
                 total_ok += 1
-                print('    > Picture {} dowloaded'.format(img_ref))
-            except AttributeError:
-                total_fail += 1
-                print('    > Picture {} skipped due to an attribute problem'.format(img_ref))
+                print('    > Picture {} already axists'.format(img_ref))
+            else:
+                img_sizes_url = '{}/{}{}'.format(base_url, img_ref, '/sizes/l/')
+                sizes_page = requests.get(img_sizes_url)
+                sizes_soup = BeautifulSoup(sizes_page.content, 'html.parser')
+                sizes = sizes_soup.find('ol', class_='sizes-list')
+                bigger_sizes = sizes.find_all('li')[-2]
+                try:
+                    biggest_size_url = 'https://flickr.com{}'.format(bigger_sizes.find('a').attrs['href'])
+                    biggest_page = requests.get(biggest_size_url)
+                    biggest_soup = BeautifulSoup(biggest_page.content, 'html.parser')
+                    img_url = biggest_soup.find('div', id='allsizes-photo').find('img').attrs['src']
+                    img_data = requests.get(img_url).content
+                    with open(img_path, 'wb') as handler:
+                        handler.write(img_data)
+                    total_ok += 1
+                    print('    > Picture {} dowloaded'.format(img_ref))
+                except AttributeError:
+                    total_fail += 1
+                    print('    > Picture {} skipped due to an attribute problem'.format(img_ref))
+
+        pictures_page = requests.get('{}/page{}'.format(album_url, page + 1))
+
     print('==> Total downloaded: {} / Total failed: {}'.format(total_ok, total_fail))
